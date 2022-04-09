469de24e2e2d09b9c3577d043ba7f43f8f23937d
MeHighAllIn <102544115+MeHighAllIn@users.noreply.github.com>
2022-04-09 22:41:15 +0300

    Create AUTENTIFICARE.java

---
+++ AUTENTIFICARE.java
@@ -0,0 +1,14 @@
+package S_R_B_A;
+
+public class AUTENTIFICARE extends CLIENT
+{
+	public String Username;
+	public String Parola;
+	public String Nume;
+	
+	@SuppressWarnings("hiding")
+	public <CLIENT> Object Autentificare() 
+	{
+		return null;
+	}
+}
